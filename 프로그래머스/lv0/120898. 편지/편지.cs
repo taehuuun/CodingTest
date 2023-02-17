e9296a60b792cbcef11bea55fef48ca73b4ca87c
LimTaeHun <43982651+taehuuun@users.noreply.github.com>
2023-02-18 05:42:45 +0900

    [level 0] Title: 편지, Time: 0.13 ms, Memory: 31.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120898. 편지/편지.cs
@@ -0,0 +1,11 @@
+using System;
+
+public class Solution {
+    public int solution(string message) {
+        
+        // message길이의 2를 곱한 값을 대입
+        int answer = message.Length * 2;
+        
+        return answer;
+    }
+}
