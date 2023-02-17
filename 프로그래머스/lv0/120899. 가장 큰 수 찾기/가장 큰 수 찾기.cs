ec4f96f0f318c6b2306c9849b33d628358992ba9
LimTaeHun <43982651+taehuuun@users.noreply.github.com>
2023-02-18 05:38:38 +0900

    Refactor: Linq와 Array.IndexOf를 활용 코드 개선

--- 프로그래머스/lv0/120899. 가장 큰 수 찾기/가장 큰 수 찾기.cs
+++ 프로그래머스/lv0/120899. 가장 큰 수 찾기/가장 큰 수 찾기.cs
@@ -1,28 +1,17 @@
 using System;
+using System.Linq;
 
 public class Solution {
     public int[] solution(int[] array) {
         int[] answer = new int[2];
         
-        // max값을 찾기 위해 초기값을 범위에 속하지 않는 작은 값으로 대입
-        int max = -99;
+        // array의 max값을 찾음
+        int max = array.Max();
         
-        // index를 찾기 위해 범위 밖인 -1 값으로 대입
-        int idx = -1;
+        // Array.Indexof를 통해 array내 max값의 인덱스를 구함
+        int idx = Array.IndexOf(array, max);
         
-        for(int i = 0; i < array.Length; i++)
-        {
-            // array의 i번째 값이 max값보다 클때
-            // max값을 array[i]의 값으로 대입
-            // idx값을 i에 대입
-            if(array[i] > max)
-            {
-                max = array[i];
-                idx = i;
-            }
-        }
-        
-        // 얻은 결과를 answer 배열에 대입
+        // 얻은 결과들을 answer에 대입
         answer[0] = max;
         answer[1] = idx;
         
