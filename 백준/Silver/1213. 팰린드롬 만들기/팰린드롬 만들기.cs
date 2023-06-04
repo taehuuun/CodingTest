e024c179c6387bca16d1eaf5a15026649c7311bf
LimTaeHun <43982651+taehuuun@users.noreply.github.com>
2023-06-05 02:36:42 +0900

    Update 팰린드롬 만들기.cs

--- 백준/Silver/1213. 팰린드롬 만들기/팰린드롬 만들기.cs
+++ 백준/Silver/1213. 팰린드롬 만들기/팰린드롬 만들기.cs
@@ -3,54 +3,30 @@ string inputStr = Console.ReadLine();
 int[] count = new int[26];
 int oddCount = 0;
 string mid = "";
-string answer = "";
+string prefix = "";
 
 for (int i = 0; i < inputStr.Length; i++)
 {
     count[inputStr[i] - 'A']++;
 }
 
-for (int i = count.Length-1; i >=0 ; i--)
+for (int i = 0; i < 26; i++)
 {
-    if (count[i] > 0)
+    if (count[i] % 2 != 0)
     {
-        if (count[i] % 2 != 0)
-        {
-            mid += (char)(i+'A');
-            count[i]--;
-            oddCount++;
-        }
-        if (oddCount >= 2)
-        {
-            Console.WriteLine("I'm Sorry Hansoo");
-            return;
-        }
-
-
-        for (int j = 0; j < count[i]; j+=2)
-        {
-            answer = (char)(i + 'A') + answer;
-            answer += (char)(i + 'A');
-        }
+        mid += (char)(i+'A');
+        oddCount++;
     }
+
+    prefix += new string((char)(i+'A'), count[i] / 2);
 }
 
-if (answer == "" && mid != "")
+if (oddCount > 1)
 {
-    Console.WriteLine(mid);
-    return;
+    Console.WriteLine("I'm Sorry Hansoo");
 }
-
-if (mid != "")
+else
 {
-    int idx = answer.Length / 2;
-    string origin = answer;
-    answer = "";
-
-    for (int i = 0; i < origin.Length; i++)
-    {
-        answer += i == idx-1 ? origin[i] + mid : origin[i];
-    }
+    string suffix = new string(prefix.Reverse().ToArray());
+    Console.WriteLine(prefix + mid + suffix);
 }
-
-Console.WriteLine(answer);
