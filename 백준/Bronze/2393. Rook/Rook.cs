aa43422245d3b3693ce4fc412183f27803dee88d
LimTaeHun <43982651+taehuuun@users.noreply.github.com>
2023-06-19 21:33:50 +0900

    [Bronze V] Title: Rook, Time: 60 ms, Memory: 5312 KB -BaekjoonHub

---
+++ 백준/Bronze/2393. Rook/Rook.cs
@@ -0,0 +1,13 @@
+Console.WriteLine("  ___  ___  ___");
+Console.WriteLine("  | |__| |__| |");
+Console.WriteLine("  |           |");
+Console.WriteLine("   \\_________/");
+Console.WriteLine("    \\_______/");
+Console.WriteLine("     |     |");
+Console.WriteLine("     |     |");
+Console.WriteLine("     |     |");
+Console.WriteLine("     |     |");
+Console.WriteLine("     |_____|");
+Console.WriteLine("  __/       \\__");
+Console.WriteLine(" /             \\");
+Console.WriteLine("/_______________\\");
