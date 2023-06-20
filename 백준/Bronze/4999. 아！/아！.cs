ef3aeb5cd5aadff328fa2aefda6b643b097ff33d
LimTaeHun <43982651+taehuuun@users.noreply.github.com>
2023-06-21 01:15:21 +0900

    [Bronze V] Title: 아!, Time: 64 ms, Memory: 5368 KB -BaekjoonHub

---
+++ 백준/Bronze/4999. 아！/아！.cs
@@ -0,0 +1,11 @@
+string jae = Console.ReadLine();
+string doctor = Console.ReadLine();
+
+if (jae.Length >= doctor.Length)
+{
+    Console.WriteLine("go");
+}
+else
+{
+    Console.WriteLine("no");
+}
