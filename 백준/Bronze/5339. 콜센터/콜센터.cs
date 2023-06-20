b9825eb24fe238d53a7db17686b1be5192c8e95e
LimTaeHun <43982651+taehuuun@users.noreply.github.com>
2023-06-21 01:21:24 +0900

    [Bronze V] Title: 콜센터, Time: 52 ms, Memory: 5304 KB -BaekjoonHub

---
+++ 백준/Bronze/5339. 콜센터/콜센터.cs
@@ -0,0 +1,10 @@
+Console.WriteLine(@"     /~\");
+Console.WriteLine(@"    ( oo|");
+Console.WriteLine(@"    _\=/_");
+Console.WriteLine(@"   /  _  \");
+Console.WriteLine(@"  //|/.\|\\");
+Console.WriteLine(@" ||  \ /  ||");
+Console.WriteLine(@"============");
+Console.WriteLine(@"|          |");
+Console.WriteLine(@"|          |");
+Console.WriteLine(@"|          |");
