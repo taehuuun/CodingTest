fd68be03b56b7989e7256e264225090fed291b40
LimTaeHun <43982651+taehuuun@users.noreply.github.com>
2023-06-21 23:25:41 +0900

    [Bronze V] Title: 사칙연산, Time: 80 ms, Memory: 5500 KB -BaekjoonHub

---
+++ 백준/Bronze/10869. 사칙연산/사칙연산.cs
@@ -0,0 +1,11 @@
+string input = Console.ReadLine();
+string[] splitInput = input.Split(' ');
+
+int a = int.Parse(splitInput[0]);
+int b = int.Parse(splitInput[1]);
+
+Console.WriteLine(a+b);
+Console.WriteLine(a-b);
+Console.WriteLine(a*b);
+Console.WriteLine(a/b);
+Console.WriteLine(a%b);
