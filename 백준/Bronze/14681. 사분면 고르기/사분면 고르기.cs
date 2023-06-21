244aa6ae292336d2bf76fd4c9b30014e008d988d
LimTaeHun <43982651+taehuuun@users.noreply.github.com>
2023-06-22 00:38:33 +0900

    [Bronze V] Title: 사분면 고르기, Time: 60 ms, Memory: 5472 KB -BaekjoonHub

---
+++ 백준/Bronze/14681. 사분면 고르기/사분면 고르기.cs
@@ -0,0 +1,19 @@
+int x = int.Parse(Console.ReadLine());
+int y = int.Parse(Console.ReadLine());
+
+if (x > 0 && y > 0)
+{
+    Console.WriteLine("1");
+}
+else if (x < 0 && y > 0)
+{
+    Console.WriteLine("2");
+}
+else if (x < 0 && y < 0)
+{
+    Console.WriteLine("3");
+}
+else
+{
+    Console.WriteLine("4");
+}
