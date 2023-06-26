9053eb22c400528d963efb7f95e2781efabb2a33
LimTaeHun <43982651+taehuuun@users.noreply.github.com>
2023-06-27 01:36:45 +0900

    [Bronze V] Title: РАВЕНСТВО, Time: 64 ms, Memory: 5640 KB -BaekjoonHub

---
+++ 백준/Bronze/24309. РАВЕНСТВО/РАВЕНСТВО.cs
@@ -0,0 +1,7 @@
+using System.Numerics;
+
+BigInteger a = BigInteger.Parse(Console.ReadLine());
+BigInteger b = BigInteger.Parse(Console.ReadLine());
+BigInteger c = BigInteger.Parse(Console.ReadLine());
+
+Console.WriteLine((b-c)/a);
