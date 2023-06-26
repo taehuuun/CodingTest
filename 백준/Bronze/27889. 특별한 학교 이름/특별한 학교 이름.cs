14350d157ee3b54bf0d17962204b64886eff7789
LimTaeHun <43982651+taehuuun@users.noreply.github.com>
2023-06-27 05:26:35 +0900

    [Bronze V] Title: 특별한 학교 이름, Time: 60 ms, Memory: 5368 KB -BaekjoonHub

---
+++ 백준/Bronze/27889. 특별한 학교 이름/특별한 학교 이름.cs
@@ -0,0 +1,17 @@
+string input = Console.ReadLine();
+
+switch (input)
+{
+    case "NLCS":
+        Console.WriteLine("North London Collegiate School");
+        break;
+    case "BHA":
+        Console.WriteLine("Branksome Hall Asia");
+        break;
+    case "KIS":
+        Console.WriteLine("Korea International School");
+        break;
+    case "SJA":
+        Console.WriteLine("St. Johnsbury Academy");
+        break;
+}
