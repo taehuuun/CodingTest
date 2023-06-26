fc26afe7859d0df025bde2bf1b327131a9b37b79
LimTaeHun <43982651+taehuuun@users.noreply.github.com>
2023-06-27 00:12:33 +0900

    [Bronze V] Title: 母音を数える (Counting Vowels), Time: 64 ms, Memory: 5440 KB -BaekjoonHub

---
+++ 백준/Bronze/18409. 母音を数える （Counting Vowels）/母音を数える （Counting Vowels）.cs
@@ -0,0 +1,14 @@
+Console.ReadLine();
+
+string input = Console.ReadLine();
+int count = 0;
+
+for (int i = 0; i < input.Length; i++)
+{
+    if (input[i] == 'a' || input[i] == 'e' || input[i] == 'i' || input[i] == 'o' || input[i] == 'u')
+    {
+        count++;
+    }
+}
+
+Console.WriteLine(count);
