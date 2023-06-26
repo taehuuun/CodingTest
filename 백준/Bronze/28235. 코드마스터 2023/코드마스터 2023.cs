79f845c15795cae6691de88f49657f30580b6655
LimTaeHun <43982651+taehuuun@users.noreply.github.com>
2023-06-27 05:54:33 +0900

    [Bronze V] Title: 코드마스터 2023, Time: 60 ms, Memory: 5360 KB -BaekjoonHub

---
+++ 백준/Bronze/28235. 코드마스터 2023/코드마스터 2023.cs
@@ -0,0 +1,17 @@
+string input = Console.ReadLine();
+
+switch (input)
+{
+    case "SONGDO":
+        Console.WriteLine("HIGHSCHOOL");
+        break;
+    case "CODE":
+        Console.WriteLine("MASTER");
+        break;
+    case "2023":
+        Console.WriteLine("0611");
+        break;
+    case "ALGORITHM":
+        Console.WriteLine("CONTEST");
+        break;
+}
