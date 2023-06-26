f4b4f65d25fdf253432f9aec6b0fce0743655843
LimTaeHun <43982651+taehuuun@users.noreply.github.com>
2023-06-27 04:28:28 +0900

    [Bronze V] Title: Serca, Time: 4120 ms, Memory: 6588 KB -BaekjoonHub

---
+++ 백준/Bronze/26766. Serca/Serca.cs
@@ -0,0 +1,16 @@
+string output = 
+ @" @@@   @@@ 
+@   @ @   @
+@    @    @
+@         @
+ @       @ 
+  @     @  
+   @   @   
+    @ @    
+     @     ";
+int n = int.Parse(Console.ReadLine());
+
+for (int i = 0; i < n; i++)
+{
+   Console.WriteLine(output);
+}    
