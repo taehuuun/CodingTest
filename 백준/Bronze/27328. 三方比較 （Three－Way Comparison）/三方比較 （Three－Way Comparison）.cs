d382b2ca8b4c93dfa117b7db1509a8f9c7e19b35
LimTaeHun <43982651+taehuuun@users.noreply.github.com>
2023-06-27 04:50:23 +0900

    [Bronze V] Title: 三方比較 (Three-Way Comparison), Time: 60 ms, Memory: 5460 KB -BaekjoonHub

---
+++ 백준/Bronze/27328. 三方比較 （Three－Way Comparison）/三方比較 （Three－Way Comparison）.cs
@@ -0,0 +1,15 @@
+    int a = int.Parse(Console.ReadLine());
+    int b = int.Parse(Console.ReadLine());
+
+    if (a == b)
+    {
+        Console.WriteLine("0");
+    }
+    else if (a < b)
+    {
+        Console.WriteLine("-1");
+    }
+    else
+    {
+        Console.WriteLine("1");
+    }
