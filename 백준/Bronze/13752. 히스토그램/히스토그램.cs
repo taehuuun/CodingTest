b04fde16c434822921603c9f5768ed4a49608b78
LimTaeHun <43982651+taehuuun@users.noreply.github.com>
2023-07-06 21:53:14 +0900

    [Bronze IV] Title: 히스토그램, Time: 424 ms, Memory: 6600 KB -BaekjoonHub

---
+++ 백준/Bronze/13752. 히스토그램/히스토그램.cs
@@ -0,0 +1,12 @@
+int n = int.Parse(Console.ReadLine());
+
+for (int i = 0; i < n; i++)
+{
+    int k = int.Parse(Console.ReadLine());
+
+    for (int j = 0; j < k; j++)
+    {
+        Console.Write("=");
+    }
+    Console.WriteLine();
+}
