a3b7583ee6371929f73e78469674b390a01fbc98
LimTaeHun <43982651+taehuuun@users.noreply.github.com>
2023-07-07 08:17:15 +0900

    [Bronze IV] Title: Rectangles, Time: 76 ms, Memory: 5540 KB -BaekjoonHub

---
+++ 백준/Bronze/15232. Rectangles/Rectangles.cs
@@ -0,0 +1,11 @@
+int y = int.Parse(Console.ReadLine());
+int x = int.Parse(Console.ReadLine());
+
+for (int i = 0; i < y; i++)
+{
+    for (int j = 0; j < x; j++)
+    {
+        Console.Write("*");
+    }
+    Console.WriteLine();
+}
