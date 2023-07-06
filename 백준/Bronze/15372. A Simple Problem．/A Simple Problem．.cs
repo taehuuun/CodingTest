0eca2cb34df47d2eeac33b4a01be05e5e4718fe4
LimTaeHun <43982651+taehuuun@users.noreply.github.com>
2023-07-07 08:47:23 +0900

    [Bronze IV] Title: A Simple Problem., Time: 268 ms, Memory: 25880 KB -BaekjoonHub

---
+++ 백준/Bronze/15372. A Simple Problem．/A Simple Problem．.cs
@@ -0,0 +1,15 @@
+using System.Numerics;
+using System.Text;
+
+StreamReader streamReader = new StreamReader(Console.OpenStandardInput());
+TextWriter stringWriter = new StreamWriter(Console.OpenStandardOutput()) { AutoFlush = true };
+StringBuilder stringBuilder = new StringBuilder();
+
+int t = int.Parse(streamReader.ReadLine());
+for (int i = 0; i < t ; i++)
+{
+    int n = int.Parse(streamReader.ReadLine());
+    stringBuilder.AppendLine(((BigInteger)n * n).ToString());
+}
+
+stringWriter.WriteLine(stringBuilder.ToString());
