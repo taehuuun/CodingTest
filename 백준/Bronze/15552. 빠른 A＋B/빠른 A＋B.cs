f6b84af6c0a2c8d754ffa74a9601e1759cc7888a
LimTaeHun <43982651+taehuuun@users.noreply.github.com>
2023-07-09 06:21:11 +0900

    [Bronze IV] Title: 빠른 A+B, Time: 520 ms, Memory: 36700 KB -BaekjoonHub

--- 백준/Bronze/15552. 빠른 A＋B/빠른 A＋B.cs
+++ 백준/Bronze/15552. 빠른 A＋B/빠른 A＋B.cs
@@ -1,14 +1,17 @@
 using System.Text;
 
 StreamReader sr = new StreamReader(new BufferedStream(Console.OpenStandardInput()));
-StreamWriter sw = new StreamWriter(new BufferedStream(Console.OpenStandardOutput())) {AutoFlush = true};
 StringBuilder sb = new StringBuilder();
+
 int n = int.Parse(sr.ReadLine());
 
-for (int i = 0; i < n; i++)
+using (StreamWriter sw = new StreamWriter(new BufferedStream(Console.OpenStandardOutput())){AutoFlush = true})
 {
-    int[] inputs = Array.ConvertAll(sr.ReadLine().Split(' '), int.Parse);
-    sb.AppendLine((inputs[0] + inputs[1]).ToString());
-}
+    for (int i = 0; i < n; i++)
+    {
+        int[] inputs = Array.ConvertAll(sr.ReadLine().Split(' '), int.Parse);
+        sb.AppendLine((inputs[0] + inputs[1]).ToString());
+    }
 
-sw.WriteLine(sb.ToString());
+    sw.WriteLine(sb.ToString());
+}
