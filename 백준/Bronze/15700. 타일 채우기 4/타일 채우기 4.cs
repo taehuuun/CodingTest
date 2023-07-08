b495fddc5aafcfb88fa26b5f4a01b6556b8094ad
LimTaeHun <43982651+taehuuun@users.noreply.github.com>
2023-07-09 06:56:02 +0900

    [Bronze IV] Title: 타일 채우기 4, Time: 64 ms, Memory: 5496 KB -BaekjoonHub

--- 백준/Bronze/15700. 타일 채우기 4/타일 채우기 4.cs
+++ 백준/Bronze/15700. 타일 채우기 4/타일 채우기 4.cs
@@ -1,3 +1,3 @@
-long[] input = Array.ConvertAll(Console.ReadLine().Split(' '),long.Parse);
+long[] input = Array.ConvertAll(Console.ReadLine().Split(),long.Parse);
 
-Console.WriteLine($"{(int)input[0] * input[1] / 2}");
+Console.WriteLine((int)input[0] * input[1] / 2);
