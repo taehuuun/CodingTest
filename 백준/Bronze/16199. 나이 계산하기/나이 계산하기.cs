e04538cc083f7c4e0de9887803934bedf3245cb2
LimTaeHun <43982651+taehuuun@users.noreply.github.com>
2023-07-09 10:17:52 +0900

    [Bronze IV] Title: 나이 계산하기, Time: 64 ms, Memory: 5480 KB -BaekjoonHub

--- 백준/Bronze/16199. 나이 계산하기/나이 계산하기.cs
+++ 백준/Bronze/16199. 나이 계산하기/나이 계산하기.cs
@@ -1,8 +1,14 @@
 int[] birth = Array.ConvertAll(Console.ReadLine().Split(),int.Parse);
 int[] today = Array.ConvertAll(Console.ReadLine().Split(),int.Parse);
+
+int birthMon = birth[1];
+int todayMon = today[1];
+int birthDay = birth[2];
+int todayDay = today[2];
+
 int annual = today[0] - birth[0];
 int count = annual + 1;
-int america = today[1] > birth[1] || today[1] == birth[1] &&today[2] >= birth[2] ? count - 1 : count-2;
+int america = todayMon > birthMon || todayMon == birthMon &&todayDay >= birthDay ? count - 1 : count-2;
 
 Console.WriteLine(america.ToString());
 Console.WriteLine(count.ToString());
