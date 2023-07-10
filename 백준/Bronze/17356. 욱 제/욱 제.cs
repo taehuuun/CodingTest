c70d042d6580a205d0d5dd0e433fbb5bdc07979b
LimTaeHun <43982651+taehuuun@users.noreply.github.com>
2023-07-11 06:01:10 +0900

    [Bronze IV] Title: 욱 제, Time: 60 ms, Memory: 5496 KB -BaekjoonHub

--- 백준/Bronze/17356. 욱 제/욱 제.cs
+++ 백준/Bronze/17356. 욱 제/욱 제.cs
@@ -1,6 +1,8 @@
-int[] inputData = Array.ConvertAll(Console.ReadLine().Split(),int.Parse);
+string[] input = Console.ReadLine().Split();
+int a = int.Parse(input[0]);
+int b = int.Parse(input[1]);
 
-double m = (double)(inputData[1] - inputData[0]) / 400;
+double m = (double)(b - a) / 400;
 double win = 1 / (1 + Math.Pow(10, m));
 
 Console.WriteLine(win);
