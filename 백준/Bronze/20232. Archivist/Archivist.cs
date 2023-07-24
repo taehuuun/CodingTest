2aca905f9dafb02622d79816ed60fd9934f23fc4
LimTaeHun <43982651+taehuuun@users.noreply.github.com>
2023-07-24 13:47:58 +0900

    [Bronze IV] Title: Archivist, Time: 60 ms, Memory: 5440 KB -BaekjoonHub

---
+++ 백준/Bronze/20232. Archivist/Archivist.cs
@@ -0,0 +1,31 @@
+string[] list ={
+    "ITMO",
+    "SPbSU",
+    "SPbSU",
+    "ITMO",
+    "ITMO",
+    "SPbSU",
+    "ITMO",
+    "ITMO",
+    "ITMO",
+    "ITMO",
+    "ITMO",
+    "PetrSU, ITMO",
+    "SPbSU",
+    "SPbSU",
+    "ITMO",
+    "ITMO",
+    "ITMO",
+    "ITMO",
+    "SPbSU",
+    "ITMO",
+    "ITMO",
+    "ITMO",
+    "ITMO",
+    "SPbSU",
+    "ITMO"
+};
+
+int year = int.Parse(Console.ReadLine());
+
+Console.WriteLine(list[year - 1995]);
