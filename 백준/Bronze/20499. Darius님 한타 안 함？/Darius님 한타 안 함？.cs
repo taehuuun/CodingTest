98296cb681705a20bff17d774a5e322d58d1f1a1
LimTaeHun <43982651+taehuuun@users.noreply.github.com>
2023-07-24 15:25:58 +0900

    [Bronze IV] Title: Darius님 한타 안 함?, Time: 60 ms, Memory: 5484 KB -BaekjoonHub

--- 백준/Bronze/20499. Darius님 한타 안 함？/Darius님 한타 안 함？.cs
+++ 백준/Bronze/20499. Darius님 한타 안 함？/Darius님 한타 안 함？.cs
@@ -1,10 +1,3 @@
 int[] kda = Array.ConvertAll(Console.ReadLine().Split('/'),int.Parse);
 
-if (kda[0] + kda[2] < kda[1] || kda[1] == 0)
-{
-    Console.WriteLine("hasu");
-}
-else
-{
-    Console.WriteLine("gosu");
-}
+Console.WriteLine((kda[0] + kda[2] < kda[1] || kda[1] == 0) ? "hasu" : "gosu");
