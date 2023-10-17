be142780bc365b6ead948160a500b147f5c1a16e
LimTaeHun <43982651+taehuuun@users.noreply.github.com>
2023-10-17 23:01:06 +0900

    [level unrated] Title: 문자열 곱하기, Time: 0.58 ms, Memory: 31.3 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181940. 문자열 곱하기/문자열 곱하기.cs
@@ -0,0 +1,14 @@
+using System;
+
+public class Solution {
+    public string solution(string my_string, int k) {
+        string answer = "";
+        
+        for (int i = 0; i < k; i++)
+        {
+            answer += my_string;
+        }
+        
+        return answer;
+    }
+}
