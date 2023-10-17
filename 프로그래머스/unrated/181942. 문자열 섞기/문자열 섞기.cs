d8fbd51281e2338df5f027cfd7c5ab5e17869069
LimTaeHun <43982651+taehuuun@users.noreply.github.com>
2023-10-17 22:49:09 +0900

    [level unrated] Title: 문자열 섞기, Time: 0.29 ms, Memory: 30.8 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181942. 문자열 섞기/문자열 섞기.cs
@@ -0,0 +1,15 @@
+using System;
+
+public class Solution {
+    public string solution(string str1, string str2) {
+        string answer = "";
+        
+        for (int i = 0; i < str1.Length; i++)
+        {
+            answer += str1[i];
+            answer += str2[i];
+        }
+        
+        return answer;
+    }
+}
