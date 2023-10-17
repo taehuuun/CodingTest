55f431e808ed15064dd9dc8cde6da2629a5340d5
LimTaeHun <43982651+taehuuun@users.noreply.github.com>
2023-10-18 07:52:32 +0900

    [Silver II] Title: 스택 수열, Time: 152 ms, Memory: 10864 KB -BaekjoonHub

--- 백준/Silver/1874. 스택 수열/스택 수열.cs
+++ 백준/Silver/1874. 스택 수열/스택 수열.cs
@@ -1,46 +1,48 @@
-using System.Text;
-
 int n = int.Parse(Console.ReadLine());
 int[] targetSequence = new int[n];
 
-StringBuilder sb = new StringBuilder();
-Stack<int> stack = new Stack<int>();
-int max = 0;
-
-for (int i = 0; i < targetSequence.Length; i++)
+for (int i = 0; i < n; i++)
 {
     targetSequence[i] = int.Parse(Console.ReadLine());
 }
 
-for (int i = 0; i < targetSequence.Length; i++)
-{
-    int target = targetSequence[i];
+string result = GetStackOperations(targetSequence);
 
-    while (true)
+if (result != null)
+{
+    Console.WriteLine(result);
+}
+else
+{
+    Console.WriteLine("NO");
+}
+
+static string GetStackOperations(int[] targetSequence)
+{
+    char[] operations = new char[targetSequence.Length * 4];  // 최대 2n의 연산이 필요합니다.
+    Stack<int> stack = new Stack<int>(targetSequence.Length);  // 스택의 초기 크기를 설정합니다.
+    int currentNumber = 1, opIndex = 0;
+
+    foreach (int target in targetSequence)
     {
-        if ((stack.Count == 0 && max < target) || (stack.Count >0 && stack.Peek() < target && max < target))
+        while (currentNumber <= target)
         {
-            sb.AppendLine("+");
-            stack.Push(max+1);
-            max = Math.Max(stack.Peek(), max);
+            stack.Push(currentNumber++);
+            operations[opIndex++] = '+';
+            operations[opIndex++] = '\n';
         }
-        else if (stack.Count >0 && stack.Peek() == target)
+
+        if (stack.Count > 0 && stack.Peek() == target)
         {
-            sb.AppendLine("-");
             stack.Pop();
-            break;
-        }
-        else if(stack.Count >0 && stack.Peek() > target)
-        {
-            sb.AppendLine("-");
-            stack.Pop();
+            operations[opIndex++] = '-';
+            operations[opIndex++] = '\n';
         }
         else
         {
-            Console.WriteLine("NO");
-            return;
+            return null;  // Invalid sequence
         }
     }
-}
 
-Console.WriteLine(sb);
+    return new string(operations, 0, opIndex);
+}
