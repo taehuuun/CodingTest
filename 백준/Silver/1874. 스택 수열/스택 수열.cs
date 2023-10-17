c867dd2747baaa61320d4acf50d82e3c79a9cfe4
LimTaeHun <43982651+taehuuun@users.noreply.github.com>
2023-10-18 08:08:38 +0900

    [Silver II] Title: 스택 수열, Time: 92 ms, Memory: 10108 KB -BaekjoonHub

--- 백준/Silver/1874. 스택 수열/스택 수열.cs
+++ 백준/Silver/1874. 스택 수열/스택 수열.cs
@@ -1,52 +1,32 @@
-using (StreamReader sr = new StreamReader(Console.OpenStandardInput()))
-using (StreamWriter sw = new StreamWriter(Console.OpenStandardOutput()) { AutoFlush = true })
-{
-    int n = int.Parse(sr.ReadLine());
-    int[] targetSequence = new int[n];
+using System.Text;
 
-    for (int i = 0; i < n; i++)
-    {
-        targetSequence[i] = int.Parse(sr.ReadLine());
+StreamReader sr = new StreamReader(new BufferedStream(Console.OpenStandardInput()));
+StreamWriter sw = new StreamWriter(new BufferedStream(Console.OpenStandardOutput()));
+int n = int.Parse(sr.ReadLine());
+int cur = 1;
+Stack<int> stack = new Stack<int>();
+StringBuilder sb = new StringBuilder();
+int check = 0;
+for(int i = 0; i < n; i++){
+    int input = int.Parse(sr.ReadLine());
+    while(cur <= input){
+        stack.Push(cur);
+        cur++;
+        sb.AppendLine("+");
     }
-
-    string result = GetStackOperations(targetSequence);
-
-    if (result != null)
-    {
-        sw.Write(result);
+    if(stack.Peek() == input){
+        stack.Pop();
+        sb.AppendLine("-");
     }
-    else
-    {
-        sw.WriteLine("NO");
+    else{
+        check = 1;
     }
 }
-
-static string GetStackOperations(int[] targetSequence)
-{
-    char[] operations = new char[targetSequence.Length * 4];  // 최대 2n의 연산이 필요합니다.
-    Stack<int> stack = new Stack<int>(targetSequence.Length);  // 스택의 초기 크기를 설정합니다.
-    int currentNumber = 1, opIndex = 0;
-
-    foreach (int target in targetSequence)
-    {
-        while (currentNumber <= target)
-        {
-            stack.Push(currentNumber++);
-            operations[opIndex++] = '+';
-            operations[opIndex++] = '\n';
-        }
-
-        if (stack.Count > 0 && stack.Peek() == target)
-        {
-            stack.Pop();
-            operations[opIndex++] = '-';
-            operations[opIndex++] = '\n';
-        }
-        else
-        {
-            return null;  // Invalid sequence
-        }
-    }
-
-    return new string(operations, 0, opIndex);
+if(check == 1){
+    sw.WriteLine("NO");
 }
+else{
+    sw.WriteLine(sb);
+}
+sr.Close();
+sw.Close();
