46dbed32e58380c4c85abfbc4adf67ab939835ae
LimTaeHun <43982651+taehuuun@users.noreply.github.com>
2023-11-26 18:36:48 +0900

    [Bronze V] Title: 정보보호학부 동아리 소개, Time: 76 ms, Memory: 5372 KB -BaekjoonHub

---
+++ 백준/Bronze/28691. 정보보호학부 동아리 소개/정보보호학부 동아리 소개.cs
@@ -0,0 +1,20 @@
+string input = Console.ReadLine();
+
+switch (input)
+{
+    case "M":
+        Console.WriteLine("MatKor");
+        break;
+    case "W":
+        Console.WriteLine("WiCys");
+        break;
+    case "C":
+        Console.WriteLine("CyKor");
+        break;
+    case "A":
+        Console.WriteLine("AlKor");
+        break;
+    case "$":
+        Console.WriteLine("$clear");
+        break;
+}
