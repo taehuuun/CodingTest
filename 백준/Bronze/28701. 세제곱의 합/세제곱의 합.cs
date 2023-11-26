c97973feaa29873c8a785fd1ba65a50ccc57657c
LimTaeHun <43982651+taehuuun@users.noreply.github.com>
2023-11-26 18:41:22 +0900

    [Bronze V] Title: 세제곱의 합, Time: 76 ms, Memory: 5528 KB -BaekjoonHub

---
+++ 백준/Bronze/28701. 세제곱의 합/세제곱의 합.cs
@@ -0,0 +1,12 @@
+int n =int.Parse(Console.ReadLine());
+
+int sum = 0;
+
+for (int i = 1; i <= n; i++)
+{
+    sum += i;
+}
+
+Console.WriteLine(sum);
+Console.WriteLine(Math.Pow(sum,2));
+Console.WriteLine(Math.Pow(sum,2));
