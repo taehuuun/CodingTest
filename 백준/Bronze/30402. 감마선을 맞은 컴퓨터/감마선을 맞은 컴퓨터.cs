1c84a51d1b73d167f6822b93da794c2c44dbcc19
LimTaeHun <43982651+taehuuun@users.noreply.github.com>
2023-11-26 14:21:48 +0900

    [Bronze IV] Title: 감마선을 맞은 컴퓨터, Time: 64 ms, Memory: 5380 KB -BaekjoonHub

---
+++ 백준/Bronze/30402. 감마선을 맞은 컴퓨터/감마선을 맞은 컴퓨터.cs
@@ -0,0 +1,22 @@
+for (var i = 0; i < 15; i++)
+{
+    string input = Console.ReadLine();
+
+    if (input.Contains("w"))
+    {
+        Console.WriteLine("chunbae");
+        return;
+    }
+
+    if (input.Contains("b"))
+    {
+        Console.WriteLine("nabi");
+        return;
+    }
+
+    if (input.Contains("g"))
+    {
+        Console.WriteLine("yeongcheol");
+        return;
+    }
+}
