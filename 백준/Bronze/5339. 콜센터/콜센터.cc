5cd367e2324014296e9cb072efcbd228500a013f
LimTaeHun <43982651+taehuuun@users.noreply.github.com>
2024-03-22 16:57:00 +0900

    [Bronze V] Title: 콜센터, Time: 0 ms, Memory: 2016 KB -BaekjoonHub

---
+++ 백준/Bronze/5339. 콜센터/콜센터.cc
@@ -0,0 +1,32 @@
+#include <iostream>
+
+using namespace std;
+
+/*
+     /~\\
+    ( oo|
+    _\\=/_
+   /  _  \\
+  //|/.\\|\\\\
+ ||  \\ /  ||
+============
+|          |
+|          |
+|          |
+*/
+
+int main()
+{
+    cout << "     /~\\"<<endl;
+    cout << "    ( oo|"<<endl;
+    cout << "    _\\=/_"<<endl;
+    cout << "   /  _  \\"<<endl;
+    cout << "  //|/.\\|\\\\"<<endl;
+    cout << " ||  \\ /  ||"<<endl;
+    cout << "============"<<endl;
+    cout << "|          |"<<endl;
+    cout << "|          |"<<endl;
+    cout << "|          |"<<endl;
+
+    return 0;
+}
