c58a2c5dde916ec93ee1ed45bb38b6bdccbef771
LimTaeHun <43982651+taehuuun@users.noreply.github.com>
2024-03-22 16:18:53 +0900

    [Bronze V] Title: 웰컴, Time: 0 ms, Memory: 2016 KB -BaekjoonHub

---
+++ 백준/Bronze/5337. 웰컴/웰컴.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+/*
+.  .   .
+|  | _ | _. _ ._ _  _
+|/\|(/.|(_.(_)[ | )(/.
+*/
+int main()
+{
+    cout << ".  .   ." <<endl;
+    cout << "|  | _ | _. _ ._ _  _" <<endl;
+    cout << "|/\\|(/.|(_.(_)[ | )(/." <<endl;
+}
