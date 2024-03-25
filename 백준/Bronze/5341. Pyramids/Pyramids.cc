864d7fa5a42ceef627de545e7d75dd821888bb64
LimTaeHun <43982651+taehuuun@users.noreply.github.com>
2024-03-26 02:18:28 +0900

    [Bronze V] Title: Pyramids, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/5341. Pyramids/Pyramids.cc
@@ -0,0 +1,29 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	while (true)
+	{
+		int n;
+
+		cin >> n;
+
+		if (n == 0)
+		{
+			break;
+		}
+
+		int sum = 0;
+
+		for (int i = 1; i <= n; i++)
+		{
+			sum += i;
+		}
+
+		cout << sum << endl;
+	}
+
+	return 0;
+}
