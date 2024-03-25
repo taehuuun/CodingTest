7b1a64f0f489a15e85ea994e717809d69c05d71a
LimTaeHun <43982651+taehuuun@users.noreply.github.com>
2024-03-26 02:56:30 +0900

    [Bronze V] Title: Can you add this?, Time: 4 ms, Memory: 2016 KB -BaekjoonHub

---
+++ 백준/Bronze/7891. Can you add this？/Can you add this？.cc
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int t;
+
+int main()
+{
+    cin >> t;
+    for(int i = 0 ; i< t; i++)
+    {
+        long long a ,b;
+
+        cin >> a >> b;
+        cout << a+ b<<endl;
+    }
+
+
+
+    return 0;
+}
