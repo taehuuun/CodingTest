35c952ffa1ac6043a05db5f054aa940d81ac1771
LimTaeHun <43982651+taehuuun@users.noreply.github.com>
2024-10-21 15:34:35 +0900

    [Bronze V] Title: SUAPC 2024 Winter, Time: 48 ms, Memory: 5484 KB -BaekjoonHub

---
+++ 백준/Bronze/32154. SUAPC 2024 Winter/SUAPC 2024 Winter.cs
@@ -0,0 +1,45 @@
+int n = int.Parse(Console.ReadLine());
+
+switch (n)
+{
+    case 1:
+        Console.WriteLine(11);
+        Console.WriteLine("A B C D E F G H J L M");
+        break;
+    case 2:
+        Console.WriteLine(9);
+        Console.WriteLine("A C E F G H I L M");
+        break;
+    case 3:
+        Console.WriteLine(9);
+        Console.WriteLine("A C E F G H I L M");
+        break;
+    case 4:
+        Console.WriteLine(9);
+        Console.WriteLine("A B C E F G H L M");
+        break;
+    case 5:
+        Console.WriteLine(8);
+        Console.WriteLine("A C E F G H L M");
+        break;
+    case 6:
+        Console.WriteLine(8);
+        Console.WriteLine("A C E F G H L M");
+        break;
+    case 7:
+        Console.WriteLine(8);
+        Console.WriteLine("A C E F G H L M");
+        break;
+    case 8:
+        Console.WriteLine(8);
+        Console.WriteLine("A C E F G H L M");
+        break;
+    case 9:
+        Console.WriteLine(8);
+        Console.WriteLine("A C E F G H L M");
+        break;
+    case 10:
+        Console.WriteLine(8);
+        Console.WriteLine("A B C F G H L M");
+        break;
+}
