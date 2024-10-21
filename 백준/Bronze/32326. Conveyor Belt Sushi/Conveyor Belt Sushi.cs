69478a76a4a782aae1b1fc17aa20ab2d27366191
LimTaeHun <43982651+taehuuun@users.noreply.github.com>
2024-10-21 15:59:25 +0900

    [Bronze V] Title: Conveyor Belt Sushi, Time: 48 ms, Memory: 5480 KB -BaekjoonHub

---
+++ 백준/Bronze/32326. Conveyor Belt Sushi/Conveyor Belt Sushi.cs
@@ -0,0 +1,7 @@
+int red = int.Parse(Console.ReadLine());
+int green = int.Parse(Console.ReadLine());
+int blue = int.Parse(Console.ReadLine());
+
+int totalRed = red * 3 + green *4 + blue *5;
+
+Console.WriteLine(totalRed);
