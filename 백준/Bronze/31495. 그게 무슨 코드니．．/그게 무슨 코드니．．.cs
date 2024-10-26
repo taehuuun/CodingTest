3b707ce48bc19f9723ba75c7a50af0809c457b84
LimTaeHun <43982651+taehuuun@users.noreply.github.com>
2024-10-27 07:09:19 +0900

    [Bronze IV] Title: 그게 무슨 코드니.., Time: 48 ms, Memory: 5372 KB -BaekjoonHub

--- 백준/Bronze/31495. 그게 무슨 코드니．．/그게 무슨 코드니．．.cs
+++ 백준/Bronze/31495. 그게 무슨 코드니．．/그게 무슨 코드니．．.cs
@@ -2,13 +2,7 @@ string input = Console.ReadLine();
 
 string[] splitInput = input.Split('"');
 
-if (splitInput.Length < 3)
-{
-    Console.WriteLine("CE");
-    return;
-}
-
-if (!string.IsNullOrEmpty(splitInput[0]) ||string.IsNullOrEmpty(splitInput[1])|| !string.IsNullOrEmpty(splitInput[2]))
+if (splitInput.Length < 3 || !string.IsNullOrEmpty(splitInput[0]) ||string.IsNullOrEmpty(splitInput[1])|| !string.IsNullOrEmpty(splitInput[2]))
 {
     Console.WriteLine("CE");
     return;
