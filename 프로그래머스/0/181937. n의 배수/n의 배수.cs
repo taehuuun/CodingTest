00f4de31500e5cec9b814d6bcbd96e1a85dbea80
LimTaeHun <43982651+taehuuun@users.noreply.github.com>
2026-01-22 21:32:53 +0900

    [level 0] Title: n의 배수, Time: 0.23 ms, Memory: 31.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181937. n의 배수/n의 배수.cs
@@ -0,0 +1,13 @@
+using System;
+
+public class Solution 
+{
+    public int solution(int num, int n) 
+    {
+        int answer = 0;
+        
+        answer = num % n == 0 ? 1 : 0;
+        
+        return answer;
+    }
+}
