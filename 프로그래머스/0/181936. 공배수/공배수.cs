fc09596b59743bc9f004023276b20f4600b105d5
LimTaeHun <43982651+taehuuun@users.noreply.github.com>
2026-01-22 21:34:53 +0900

    [level 0] Title: 공배수, Time: 0.17 ms, Memory: 31.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/181936. 공배수/공배수.cs
@@ -0,0 +1,11 @@
+using System;
+
+public class Solution {
+    public int solution(int number, int n, int m) {
+        int answer = 0;
+        
+        answer = ((number % n == 0) && (number % m == 0)) ? 1 : 0;
+        
+        return answer;
+    }
+}
