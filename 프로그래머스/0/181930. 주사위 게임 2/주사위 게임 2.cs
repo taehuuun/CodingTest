cbe997b38ceed38b8e36b44a396e2b29c9e730e1
LimTaeHun <43982651+taehuuun@users.noreply.github.com>
2026-01-23 02:38:07 +0900

    [level 0] Title: 주사위 게임 2, Time: 0.30 ms, Memory: 31.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/181930. 주사위 게임 2/주사위 게임 2.cs
@@ -0,0 +1,23 @@
+using System;
+
+public class Solution {
+    public int solution(int a, int b, int c) 
+    {
+        int answer = 0;
+        
+        if(a == b && b ==c && a ==c)
+        {
+            answer = (a + b + c) * (a * a + b* b+ c* c) * (a * a * a + b * b * b + c * c * c);
+        }
+        else if(a != b && b != c && a != c)
+        {
+            answer = a + b + c;
+        }
+        else
+        {
+            answer = (a + b + c) * (a * a + b* b+ c* c);
+        }
+        
+        return answer;
+    }
+}
