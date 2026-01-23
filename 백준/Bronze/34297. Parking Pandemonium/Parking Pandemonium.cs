efe833621d7e0d0c07f72de8ca95f4ab3fb1874c
LimTaeHun <43982651+taehuuun@users.noreply.github.com>
2026-01-23 20:19:30 +0900

    [Bronze V] Title: Parking Pandemonium, Time: 48 ms, Memory: 5428 KB -BaekjoonHub

---
+++ 백준/Bronze/34297. Parking Pandemonium/Parking Pandemonium.cs
@@ -0,0 +1,6 @@
+var sr = new StreamReader(Console.OpenStandardInput());
+int m = int.Parse(sr.ReadLine());
+int k = int.Parse(sr.ReadLine());
+int c = int.Parse(sr.ReadLine());
+
+Console.WriteLine(m * c);
