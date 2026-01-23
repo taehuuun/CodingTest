b960ead6c6d7fc16da2cc073bb904f12e385da6b
LimTaeHun <43982651+taehuuun@users.noreply.github.com>
2026-01-23 11:32:42 +0900

    [Bronze V] Title: 가희와 4시간의 벽 1, Time: 48 ms, Memory: 5464 KB -BaekjoonHub

---
+++ 백준/Bronze/32775. 가희와 4시간의 벽 1/가희와 4시간의 벽 1.cs
@@ -0,0 +1,11 @@
+int sab = int.Parse(Console.ReadLine());
+int fab = int.Parse(Console.ReadLine());
+
+if(sab > fab)
+{
+    Console.WriteLine("flight");
+}
+else
+{
+    Console.WriteLine("high speed rail");   
+}
