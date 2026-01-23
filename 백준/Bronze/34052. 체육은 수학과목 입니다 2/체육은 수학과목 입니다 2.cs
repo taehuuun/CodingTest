4890391c3be609f3a33a0a83c809f578c9c16325
LimTaeHun <43982651+taehuuun@users.noreply.github.com>
2026-01-23 14:43:44 +0900

    [Bronze V] Title: 체육은 수학과목 입니다 2, Time: 48 ms, Memory: 5448 KB -BaekjoonHub

--- 백준/Bronze/34052. 체육은 수학과목 입니다 2/체육은 수학과목 입니다 2.cs
+++ 백준/Bronze/34052. 체육은 수학과목 입니다 2/체육은 수학과목 입니다 2.cs
@@ -3,14 +3,10 @@ var sr = new StreamReader(Console.OpenStandardInput());
 // 4줄 입력: 각 바퀴에 걸린 시간(초)
 int sum = 0;
 
-for (int i = 0; i < 4; i++)
-{
-    // 공백/개행이 섞여 들어올 가능성까지 고려하여 Trim 처리
-    int t = int.Parse(sr.ReadLine());
-
-    // 4바퀴 시간 누적
-    sum += t;
-}
+sum += int.Parse(sr.ReadLine());
+sum += int.Parse(sr.ReadLine());
+sum += int.Parse(sr.ReadLine());
+sum += int.Parse(sr.ReadLine());
 
 // 5바퀴를 1800초 이하로 완주하면 통과
 Console.WriteLine( sum + 300 <= 1800 ? "Yes" : "No");
