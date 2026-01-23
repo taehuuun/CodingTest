72ed6dcc1c021d6bf907ddb9b90c1d82d071acf5
LimTaeHun <43982651+taehuuun@users.noreply.github.com>
2026-01-23 21:02:33 +0900

    [Bronze V] Title: E-Days Ore Cart Pull, Time: 48 ms, Memory: 5424 KB -BaekjoonHub

---
+++ 백준/Bronze/34446. E－Days Ore Cart Pull/E－Days Ore Cart Pull.cs
@@ -0,0 +1,6 @@
+var sr = new StreamReader(Console.OpenStandardInput());
+var m = int.Parse(sr.ReadLine());
+var n = int.Parse(sr.ReadLine());
+var t = int.Parse(sr.ReadLine());
+
+Console.WriteLine(m * 2);
