e194ad0f4a8c78497e21068ebe682be9b9780e02
LimTaeHun <43982651+taehuuun@users.noreply.github.com>
2026-01-23 20:58:32 +0900

    [Bronze V] Title: Hack-a-Holics, Time: 48 ms, Memory: 5440 KB -BaekjoonHub

---
+++ 백준/Bronze/34443. Hack－a－Holics/Hack－a－Holics.cs
@@ -0,0 +1,6 @@
+var sr = new StreamReader(Console.OpenStandardInput());
+var n = int.Parse(sr.ReadLine());
+var c = int.Parse(sr.ReadLine());
+var p = int.Parse(sr.ReadLine());
+
+Console.WriteLine(n * p);
