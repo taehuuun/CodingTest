3c0062a446100f931641f68afeeb9dcac0f292a6
LimTaeHun <43982651+taehuuun@users.noreply.github.com>
2026-01-23 10:44:30 +0900

    [level 0] Title: n 번째 원소까지, Time: 0.23 ms, Memory: 31.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/181889. n 번째 원소까지/n 번째 원소까지.cs
@@ -0,0 +1,16 @@
+using System;
+
+public class Solution 
+{
+    public int[] solution(int[] num_list, int n) 
+    {
+        int[] answer = new int[n];
+        
+        for(int i = 0; i < n; i++)
+        {
+            answer[i] = num_list[i];
+        }
+
+        return answer;
+    }
+}
