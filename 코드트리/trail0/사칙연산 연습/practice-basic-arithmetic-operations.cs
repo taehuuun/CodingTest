13e490dcd982b514a8cad41a71108174399f2d69
LimTaeHun <43982651+taehuuun@users.noreply.github.com>
2026-06-01 15:30:28 +0900

    [Passed] 사칙연산 연습 (56ms, 5MB)

---
+++ 코드트리/trail0/사칙연산 연습/practice-basic-arithmetic-operations.cs
@@ -0,0 +1,14 @@
+using System;
+
+public class Codetree
+{  
+    public static void Main()
+    {
+        int a = 5;
+        int b = 30;
+        int c = a+b;
+
+        Console.WriteLine(c/8);
+        Console.WriteLine(c%8);
+    }
+}
