534cae69d068b8d7ace1b57eb59c0a72236ce67d
LimTaeHun <43982651+taehuuun@users.noreply.github.com>
2026-06-01 19:26:12 +0900

    [Passed] 세 실수의 반올림 (2ms, 0MB)

---
+++ 코드트리/trail0/세 실수의 반올림/rounding-of-three-actual-numbers.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+    float a,b,c;
+
+    cin >> a >> b >> c;
+
+    cout << fixed;
+    cout.precision(3);
+
+    cout << a << endl;
+    cout << b << endl;
+    cout << c << endl;
+
+    return 0;
+}
