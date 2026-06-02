e1971f3827b505a361173e83ecddc853a4488f85
LimTaeHun <43982651+taehuuun@users.noreply.github.com>
2026-06-02 12:18:20 +0900

    [level 0] Title: 배열 만들기 4, Time: 1.00 ms, Memory: 6.52 MB -BaekjoonHub

---
+++ 프로그래머스/0/181918. 배열 만들기 4/배열 만들기 4.cpp
@@ -0,0 +1,31 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(vector<int> arr) 
+{
+    vector<int> stk;
+    
+    int i = 0;
+    
+    while(i < arr.size())
+    {
+        if(stk.size() == 0)
+        {
+            stk.push_back(arr[i]);
+            i++;
+        }
+        else if(stk[stk.size()-1] < arr[i])
+        {
+            stk.push_back(arr[i]);
+            i++;
+        }
+        else if(stk[stk.size()-1] >= arr[i])
+        {
+            stk.pop_back();
+        }
+    }
+    
+    return stk;
+}
