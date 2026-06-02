8347650a4739b7b4c7e75d0602dd5a5b610a2657
LimTaeHun <43982651+taehuuun@users.noreply.github.com>
2026-06-02 10:41:09 +0900

    [Passed] 음수 구별하기 (2ms, 0MB)

---
+++ 코드트리/trail0/음수 구별하기/separate-negative-number.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+    int n = 0;
+
+    cin >> n;
+
+    cout << n << endl;
+    if(n < 0)
+    {
+        
+        cout << "minus";
+    }
+
+    return 0;
+}
