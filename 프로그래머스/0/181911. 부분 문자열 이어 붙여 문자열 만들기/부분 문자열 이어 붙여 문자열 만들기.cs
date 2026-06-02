02da2d279f516d60d49a0408b9a729482ff70cc8
LimTaeHun <43982651+taehuuun@users.noreply.github.com>
2026-06-03 05:12:21 +0900

    [level 0] Title: 부분 문자열 이어 붙여 문자열 만들기, Time: 0.57 ms, Memory: 31.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181911. 부분 문자열 이어 붙여 문자열 만들기/부분 문자열 이어 붙여 문자열 만들기.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+
+public class Solution 
+{
+    public string solution(string[] my_strings, int[,] parts) 
+    {
+        string answer = "";
+        
+        for(int i = 0; i < my_strings.Length; i++)
+        {
+            answer += my_strings[i].Substring(parts[i,0], parts[i,1] - parts[i,0] + 1);
+        }
+        
+        return answer;
+    }
+}
