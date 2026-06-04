b5fccdf546d69e7f68e6d666bc9e0950a6c03ff9
LimTaeHun <43982651+taehuuun@users.noreply.github.com>
2026-06-04 21:04:06 +0900

    [Passed] 배열의 값을 3배로 (2ms, 0MB)

---
+++ 코드트리/trail0/배열의 값을 3배로/triple-the-values-of-the-array.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+    int arr[3][3] = {};
+
+    for(int i = 0 ; i < 3; i++)
+    {
+        for(int j = 0 ; j < 3; j++)
+        {
+            cin >> arr[i][j];
+            arr[i][j] *= 3;
+        }
+    }
+
+    for(int i = 0 ; i < 3; i++)
+    {
+        for(int j = 0 ; j < 3; j++)
+        {
+            cout << arr[i][j] << " ";
+        }
+
+        cout << endl;
+    }
+
+    return 0;
+}
