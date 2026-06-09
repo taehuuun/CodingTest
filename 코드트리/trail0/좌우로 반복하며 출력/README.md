ce4a46b0cab7e30c09360e63a7f18445cbccb3eb
LimTaeHun <43982651+taehuuun@users.noreply.github.com>
2026-06-09 18:35:39 +0900

    [Passed] 좌우로 반복하며 출력 (2ms, 0MB)

---
+++ 코드트리/trail0/좌우로 반복하며 출력/README.md
@@ -0,0 +1,8 @@
+# [[챌린지]좌우로 반복하며 출력](https://www.codetree.ai/trails/complete/curated-cards/nl-pre-2d-array-pattern-printing-1)
+
+| 항목 | 내용 |
+|---|---|
+| 분류 | Trail |
+| 커리큘럼 | [Trail 0 / 2차원 배열 / 2차원 배열 패턴 출력](https://www.codetree.ai/trail-info/codetree-101/) |
+| 난이도 | 쉬움 |
+| 경험치 | 10 XP |
---
+++ 코드트리/trail0/좌우로 반복하며 출력/print-in-left-right-reverse.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+    int n;
+
+    cin >> n;
+
+    for(int i =0; i < n; i++)
+    {
+        if(i % 2 == 0)
+        {
+            for(int j = 1; j <=n; j++)
+            {
+                cout << j;
+            }
+        }
+        else
+        {
+            for(int j = n; j > 0; j--)
+            {
+                cout << j;
+            }
+        }
+
+        cout << endl;
+    }
+
+    return 0;
+}
