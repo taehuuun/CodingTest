39a5ff9e4aae6cbe6bb8227f36141d020be9a891
LimTaeHun <43982651+taehuuun@users.noreply.github.com>
2026-06-10 14:17:36 +0900

    [Passed] 다양하게 출력 (2ms, 0MB)

---
+++ 코드트리/trail1/다양하게 출력/print-in-variety.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+    cout << "Total days in Year" << endl;
+    cout << "365" << endl;
+    cout << "Circumference rate" << endl;
+    cout << "3.1415926535";
+    return 0;
+}
