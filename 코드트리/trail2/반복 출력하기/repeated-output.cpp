198184353fe840a4e856dba4fd993c5ac5ead6bb
LimTaeHun <43982651+taehuuun@users.noreply.github.com>
2026-06-13 03:04:11 +0900

    [Passed] 반복 출력하기 (2ms, 0MB)

---
+++ 코드트리/trail2/반복 출력하기/repeated-output.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+void Print(int n)
+{
+    for(int i = 0 ; i < n; i++)
+    {
+        cout << "12345^&*()_" << endl;
+    }
+}
+
+int main() 
+{
+    int a;
+
+    cin >> a;
+
+    Print(a);
+
+    return 0;
+}
