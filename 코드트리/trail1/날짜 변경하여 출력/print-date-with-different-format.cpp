e7a5bab6d203dda5b1ce949a4464790fae593768
LimTaeHun <43982651+taehuuun@users.noreply.github.com>
2026-06-30 13:30:19 +0900

    [Passed] 날짜 변경하여 출력 (2ms, 0MB)

---
+++ 코드트리/trail1/날짜 변경하여 출력/print-date-with-different-format.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int y, m, d;
+
+    cin >> y;
+    cin.get();
+    cin >> m;
+    cin.get();
+    cin >> d;
+
+    cout << m << '-' <<d <<'-' << y;
+
+    return 0;
+}
