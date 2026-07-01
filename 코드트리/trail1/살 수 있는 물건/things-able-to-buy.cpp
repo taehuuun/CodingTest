24056a89fd21759f61f9df57cf5589fc048ca014
LimTaeHun <43982651+taehuuun@users.noreply.github.com>
2026-07-02 00:22:58 +0900

    [Passed] 살 수 있는 물건 (2ms, 0MB)

---
+++ 코드트리/trail1/살 수 있는 물건/things-able-to-buy.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+    int n;
+
+    cin >> n;
+
+    if(n >= 3000)
+    {
+        cout << "book";
+    }
+    else if( n >= 1000)
+    {
+        cout << "mask";
+    }
+    else
+    {
+        cout << "no";
+    }
+
+    return 0;
+}
