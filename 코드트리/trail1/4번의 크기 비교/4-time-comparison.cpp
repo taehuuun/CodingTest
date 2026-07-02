3179f059ba4d64021f68f2949be648af88b064b9
LimTaeHun <43982651+taehuuun@users.noreply.github.com>
2026-07-02 16:47:12 +0900

    [Passed] 4번의 크기 비교 (2ms, 0MB)

---
+++ 코드트리/trail1/4번의 크기 비교/4-time-comparison.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+    int a, b, c, d, e;
+
+    cin >> a >> b >> c >> d >> e;
+
+    cout << (a > b) << endl;
+    cout << (a > c) << endl;
+    cout << (a > d) << endl;
+    cout << (a > e) << endl;
+
+    return 0;
+}
---
+++ 코드트리/trail1/4번의 크기 비교/README.md
@@ -0,0 +1,8 @@
+# [[테스트]4번의 크기 비교](https://www.codetree.ai/trails/complete/curated-cards/test-4-time-comparison)
+
+| 항목 | 내용 |
+|---|---|
+| 분류 | Trail |
+| 커리큘럼 | [Trail 1 / 조건문 / 비교 연산자와 조건문](https://www.codetree.ai/trail-info/novice-low/) |
+| 난이도 | 쉬움 |
+| 경험치 | 10 XP |
