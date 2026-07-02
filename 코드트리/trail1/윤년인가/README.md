0d7ed446b042428124250bd79627f3f9b1bf1a36
LimTaeHun <43982651+taehuuun@users.noreply.github.com>
2026-07-03 08:51:47 +0900

    [Passed] 윤년인가 (2ms, 0MB)

---
+++ 코드트리/trail1/윤년인가/README.md
@@ -0,0 +1,8 @@
+# [[개념]윤년인가](https://www.codetree.ai/trails/complete/curated-cards/intro-is-leap-year)
+
+| 항목 | 내용 |
+|---|---|
+| 분류 | Trail |
+| 커리큘럼 | [Trail 1 / 조건문 / 중첩 조건문](https://www.codetree.ai/trail-info/novice-low/) |
+| 난이도 | 쉬움 |
+| 경험치 | 10 XP |
---
+++ 코드트리/trail1/윤년인가/is-leap-year.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+    int y;
+
+    cin >> y;
+
+    if(y % 4 == 0)
+    {
+        if(y % 100 ==0 && y % 400 != 0)
+        {
+            cout << "false";
+        }
+        else
+        {
+            cout << "true";
+        }
+    }
+    else
+    {
+        cout << "false";
+    }
+
+    return 0;
+}
