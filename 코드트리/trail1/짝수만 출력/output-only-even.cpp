f26f4e25be7d372941afb7bdebecc9b7d734c2ed
LimTaeHun <43982651+taehuuun@users.noreply.github.com>
2026-07-03 21:02:55 +0900

    [Passed] 짝수만 출력 (2ms, 0MB)

---
+++ 코드트리/trail1/짝수만 출력/output-only-even.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+    int a, b;
+
+    cin >> a >> b;
+
+    while(a <= b)
+    {
+        if(a % 2 == 0)
+        {
+            cout << a << " ";
+        }
+
+        a++;
+    }
+
+    return 0;
+}
