b0b573f786b7bf74739d8e3c6f21082bef6b3a53
LimTaeHun <43982651+taehuuun@users.noreply.github.com>
2026-07-06 07:45:14 +0900

    [Passed] 100점 도달하기 (2ms, 0MB)

---
+++ 코드트리/trail1/100점 도달하기/README.md
@@ -0,0 +1,8 @@
+# [[챌린지]100점 도달하기](https://www.codetree.ai/trails/complete/curated-cards/challenge-reach-100-point)
+
+| 항목 | 내용 |
+|---|---|
+| 분류 | Trail |
+| 커리큘럼 | [Trail 1 / 단순 반복문 / 반복문 안의 if](https://www.codetree.ai/trail-info/novice-low/) |
+| 난이도 | 쉬움 |
+| 경험치 | 10 XP |
---
+++ 코드트리/trail1/100점 도달하기/reach-100-point.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+    int n;
+    cin >> n;
+
+    while(n <= 100)
+    {
+        if(n >= 90)
+        {
+            cout << "A";
+        }
+        else if(n >= 80)
+        {
+            cout << "B";
+        }
+        else if(n >= 70)
+        {
+            cout << "C";
+        }
+        else if(n >= 60)
+        {
+            cout << "D";
+        }
+        else
+        {
+            cout << "F";
+        }
+
+        cout << " ";
+        n++;
+    }
+
+    return 0;
+}
