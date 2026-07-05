1cfcc2ec7e02d89c68dd0426c85a4405760cc4af
LimTaeHun <43982651+taehuuun@users.noreply.github.com>
2026-07-05 23:37:16 +0900

    [Passed] 높은 수에서 낮은 수까지 (2ms, 0MB)

---
+++ 코드트리/trail1/높은 수에서 낮은 수까지/README.md
@@ -0,0 +1,8 @@
+# [[챌린지]높은 수에서 낮은 수까지](https://www.codetree.ai/trails/complete/curated-cards/challenge-from-high-to-low)
+
+| 항목 | 내용 |
+|---|---|
+| 분류 | Trail |
+| 커리큘럼 | [Trail 1 / 단순 반복문 / if 안의 for](https://www.codetree.ai/trail-info/novice-low/) |
+| 난이도 | 쉬움 |
+| 경험치 | 10 XP |
---
+++ 코드트리/trail1/높은 수에서 낮은 수까지/from-high-to-low.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+    int a ,b;
+
+    cin >> a >> b;
+
+    if(a < b)
+    {
+        for(int i = b; i >= a; i--)
+        {
+            cout << i << " ";
+        }
+    }
+    else
+    {
+        for(int i = a; i >= b; i--)
+        {
+            cout << i << " ";
+        }
+    }
+
+    return 0;
+}
