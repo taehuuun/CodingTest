b54db6429b67cc8cb55221e3ba71bf4635e366a8
LimTaeHun <43982651+taehuuun@users.noreply.github.com>
2026-07-06 23:15:57 +0900

    [Passed] 홀수만 더하기 (2ms, 0MB)

---
+++ 코드트리/trail1/홀수만 더하기/README.md
@@ -0,0 +1,8 @@
+# [[챌린지]홀수만 더하기](https://www.codetree.ai/trails/complete/curated-cards/challenge-add-only-odd-numbers)
+
+| 항목 | 내용 |
+|---|---|
+| 분류 | Trail |
+| 커리큘럼 | [Trail 1 / 단순 반복문 / sum 계산하기](https://www.codetree.ai/trail-info/novice-low/) |
+| 난이도 | 쉬움 |
+| 경험치 | 10 XP |
---
+++ 코드트리/trail1/홀수만 더하기/add-only-odd-numbers.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+    int n, sum = 0;
+
+    cin >> n;
+
+    for(int i = 0; i < n; i++)
+    {
+        int input;
+
+        cin >> input;
+
+        if(input % 2 != 0 && input % 3 == 0)
+        {
+            sum += input;
+        }
+    }
+
+    cout << sum;
+
+    return 0;
+}
