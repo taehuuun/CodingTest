dc18c7bef5b50747f5bbd4285dc11bc79da0e259
LimTaeHun <43982651+taehuuun@users.noreply.github.com>
2026-07-07 22:18:56 +0900

    [Passed] 소수 판별기 (1ms, 0MB)

---
+++ 코드트리/trail1/소수 판별기/README.md
@@ -0,0 +1,8 @@
+# [[챌린지]소수 판별기](https://www.codetree.ai/trails/complete/curated-cards/challenge-prime-number-discriminator)
+
+| 항목 | 내용 |
+|---|---|
+| 분류 | Trail |
+| 커리큘럼 | [Trail 1 / 단순 반복문 / 모두 만족하는 경우](https://www.codetree.ai/trail-info/novice-low/) |
+| 난이도 | 보통 |
+| 경험치 | 10 XP |
---
+++ 코드트리/trail1/소수 판별기/prime-number-discriminator.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+    int n;
+    bool isPrime = true;
+
+    cin >> n;
+
+    for(int i = 2; i < n; i++)
+    {
+        if(n % i == 0)
+        {
+            isPrime = false;
+            break;
+        }
+    }
+
+    cout << (isPrime ? "P" : "C");
+
+    return 0;
+}
