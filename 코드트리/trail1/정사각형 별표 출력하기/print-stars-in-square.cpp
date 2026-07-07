983777145d49f7bc314c9069f03f501d4c17e729
LimTaeHun <43982651+taehuuun@users.noreply.github.com>
2026-07-07 22:25:15 +0900

    [Passed] 정사각형 별표 출력하기 (2ms, 0MB)

---
+++ 코드트리/trail1/정사각형 별표 출력하기/print-stars-in-square.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+    int n;
+
+    cin >> n;
+
+    for(int y = 0; y < n; y++)
+    {
+        for(int x = 0; x < n; x++)
+        {
+            cout << "*";
+        }
+
+        cout << endl;
+    }
+
+    return 0;
+}
