a606153d344cc5b0a9303166da0e4129bfa4af9e
LimTaeHun <43982651+taehuuun@users.noreply.github.com>
2026-07-08 21:12:12 +0900

    [Passed] 별표 출력하기 4 (2ms, 0MB)

---
+++ 코드트리/trail1/별표 출력하기 4/README.md
@@ -0,0 +1,8 @@
+# [[챌린지]별표 출력하기 4](https://www.codetree.ai/trails/complete/curated-cards/challenge-print-star-4)
+
+| 항목 | 내용 |
+|---|---|
+| 분류 | Trail |
+| 커리큘럼 | [Trail 1 / 다중 반복문 / 행에 대해 대칭인 별 출력](https://www.codetree.ai/trail-info/novice-low/) |
+| 난이도 | 쉬움 |
+| 경험치 | 10 XP |
---
+++ 코드트리/trail1/별표 출력하기 4/print-star-4.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+    int n;
+
+    cin >> n;
+
+    for(int i = 0; i < n; i++)
+    {
+        for(int j = n - i; j > 0; j--)
+        {
+            cout << "* ";
+        }
+
+        cout << endl;
+    }
+
+    for(int i = 1 ; i <= n-1; i++)
+    {
+        for(int j = 0; j < i+1; j++)
+        {
+            cout << "* ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
