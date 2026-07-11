2602e63b1da3cfce409d8e53688601b4e1396a04
LimTaeHun <43982651+taehuuun@users.noreply.github.com>
2026-07-11 09:04:31 +0900

    [Passed] 별표 출력하기 6 (2ms, 0MB)

---
+++ 코드트리/trail1/별표 출력하기 6/print-star-6.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+    int n;
+
+    cin >> n;
+
+    for(int i = 0; i < n; i++)
+    {
+        for(int j = 0; j <  i; j++)
+        {
+            cout << "  ";
+        }
+
+        for(int j = 0; j < 2 * ( n- i) -1; j++)
+        {
+            cout << "* ";
+        }
+
+        cout << endl;
+    }
+
+    for(int i = 0; i < n - 1; i++)
+    {
+        for(int j = 0; j < n - i - 2; j++)
+        {
+            cout << "  ";
+        }
+
+        for(int j = 0; j < 3 + 2 * i; j++)
+        {
+            cout << "* ";
+        }
+
+        cout << endl;
+    }
+
+    return 0;
+}
