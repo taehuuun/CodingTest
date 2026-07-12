a656af6990edd0ccec6b0f056bd59f31c783c8ea
LimTaeHun <43982651+taehuuun@users.noreply.github.com>
2026-07-13 00:46:32 +0900

    [Passed] 체크 다이아몬드 (2ms, 0MB)

---
+++ 코드트리/trail1/체크 다이아몬드/check-diamond.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+    int n;
+
+    cin >> n;
+
+    for(int i = 0; i < n - 1; i++)
+    {
+        for(int j = 0; j < n - i - 1;j++)
+        {
+            cout << " ";
+        }
+
+        for(int j = 0; j < i + 1; j ++)
+        {
+            cout << "* ";
+        }
+
+        cout << endl;
+    }
+
+    for(int i = 0 ; i < n; i++)
+    {
+        cout << "* ";
+    }
+
+    cout << endl;
+
+    for(int i = 0; i < n - 1; i++)
+    {
+        for(int j = 0 ; j < i + 1; j++)
+        {
+            cout << " ";
+        }
+
+        for(int j = 0; j< n - 1 - i; j++)
+        {
+            cout << "* ";
+        }
+
+        cout << endl;
+    }
+
+    return 0;
+}
