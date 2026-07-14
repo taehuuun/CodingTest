46af54bfedf75a090d42fe12f342cb739536df66
LimTaeHun <43982651+taehuuun@users.noreply.github.com>
2026-07-14 18:58:56 +0900

    [Passed] 정상적인 구구단 (2ms, 0MB)

---
+++ 코드트리/trail1/정상적인 구구단/normal-multiple.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+    int n;
+
+    cin >> n;
+
+    for(int i = 1; i <= n; i++)
+    {
+        for(int j = 1; j <= n; j++)
+        {
+            cout << i << " * " << j << " = " << i * j;
+            if(j < n)
+            {
+                cout <<", ";
+            }
+        }
+
+        cout << endl;
+    }
+
+    return 0;
+}
