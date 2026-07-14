27cc8f4e5fee6ae5d49bdbc878f4aabb79bf1c9c
LimTaeHun <43982651+taehuuun@users.noreply.github.com>
2026-07-14 22:49:42 +0900

    [Passed] 모양대로 수 출력하기 (2ms, 0MB)

---
+++ 코드트리/trail1/모양대로 수 출력하기/print-out-numbers-in-specific-shape.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+    int n;
+
+    cin >> n;
+
+    for(int i = 0; i < n; i++)
+    {
+        for(int j = 0; j < i; j++)
+        {
+            cout << "  ";
+        }
+
+        for(int j = n - i; j > 0; j--)
+        {
+            cout << j << " ";
+        }
+
+        cout << endl;
+    }
+
+    return 0;
+}
