7ae46050ca4c20f58e1cb32e2a87e84cfd222fcc
LimTaeHun <43982651+taehuuun@users.noreply.github.com>
2026-07-14 22:23:01 +0900

    [Passed] 숫자 피라미드 2 (2ms, 0MB)

---
+++ 코드트리/trail1/숫자 피라미드 2/README.md
@@ -0,0 +1,8 @@
+# [[챌린지]숫자 피라미드 2](https://www.codetree.ai/trails/complete/curated-cards/challenge-number-pyramid-2)
+
+| 항목 | 내용 |
+|---|---|
+| 분류 | Trail |
+| 커리큘럼 | [Trail 1 / 다중 반복문 / 모양대로 숫자 출력](https://www.codetree.ai/trail-info/novice-low/) |
+| 난이도 | 쉬움 |
+| 경험치 | 10 XP |
---
+++ 코드트리/trail1/숫자 피라미드 2/number-pyramid-2.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+    int n, num = 1;
+
+    cin >> n;
+
+    for(int i = 1; i <= n; i++)
+    {
+        for(int j = 0; j < i; j++)
+        {
+            cout << num << " ";
+            num++;
+        }
+
+        cout << endl;
+    }
+
+    return 0;
+}
