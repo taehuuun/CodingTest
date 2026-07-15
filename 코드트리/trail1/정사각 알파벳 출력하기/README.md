e3a947e9ea9fd6e4db37e73e11136e1074ededd9
LimTaeHun <43982651+taehuuun@users.noreply.github.com>
2026-07-15 14:44:05 +0900

    [Passed] 정사각 알파벳 출력하기 (2ms, 0MB)

---
+++ 코드트리/trail1/정사각 알파벳 출력하기/README.md
@@ -0,0 +1,8 @@
+# [[개념]정사각 알파벳 출력하기](https://www.codetree.ai/trails/complete/curated-cards/intro-print-alpabet-in-square)
+
+| 항목 | 내용 |
+|---|---|
+| 분류 | Trail |
+| 커리큘럼 | [Trail 1 / 다중 반복문 / 알파벳 출력](https://www.codetree.ai/trail-info/novice-low/) |
+| 난이도 | 쉬움 |
+| 경험치 | 10 XP |
---
+++ 코드트리/trail1/정사각 알파벳 출력하기/print-alpabet-in-square.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+    int n, ascii = 65;
+
+    cin >> n;
+
+    for(int i = 0; i < n; i++)
+    {
+        for(int j = 0; j < n; j++)
+        {
+            cout << (char)ascii;
+            ascii++;
+        }
+
+        cout << endl;
+    }
+
+    return 0;
+}
