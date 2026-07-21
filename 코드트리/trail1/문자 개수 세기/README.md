c6174994074a9c90a9895ae22adcb90abf127004
LimTaeHun <43982651+taehuuun@users.noreply.github.com>
2026-07-21 22:57:22 +0900

    [Passed] 문자 개수 세기 (2ms, 0MB)

---
+++ 코드트리/trail1/문자 개수 세기/README.md
@@ -0,0 +1,8 @@
+# [[챌린지]문자 개수 세기](https://www.codetree.ai/trails/complete/curated-cards/challenge-count-char)
+
+| 항목 | 내용 |
+|---|---|
+| 분류 | Trail |
+| 커리큘럼 | [Trail 1 / 문자열 / 공백있는 문자열 한번에 입력받기](https://www.codetree.ai/trail-info/novice-low/) |
+| 난이도 | 쉬움 |
+| 경험치 | 10 XP |
---
+++ 코드트리/trail1/문자 개수 세기/count-char.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() 
+{
+    int count = 0;
+    string str;
+    char c;
+
+    getline(cin, str);
+    cin >> c;
+
+    for(int i = 0; i < str.length(); i++)
+    {
+        if(str[i] == c)
+        {
+            count++;
+        }
+    }
+
+    cout << count;
+
+    return 0;
+}
