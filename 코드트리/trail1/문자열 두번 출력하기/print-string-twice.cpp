99c5159471e86394e403219c4a80a009da56b135
LimTaeHun <43982651+taehuuun@users.noreply.github.com>
2026-07-21 17:13:02 +0900

    [Passed] 문자열 두번 출력하기 (2ms, 0MB)

---
+++ 코드트리/trail1/문자열 두번 출력하기/print-string-twice.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() 
+{
+    string input;
+
+    cin >> input;
+
+    cout << input << endl;
+    cout << input << endl;
+
+    return 0;
+}
