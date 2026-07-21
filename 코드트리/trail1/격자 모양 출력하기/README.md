555c869f1156d1bdb114858299fefbdbe42cc655
LimTaeHun <43982651+taehuuun@users.noreply.github.com>
2026-07-21 15:45:46 +0900

    [Passed] 격자 모양 출력하기 (2ms, 0MB)

---
+++ 코드트리/trail1/격자 모양 출력하기/README.md
@@ -0,0 +1,8 @@
+# [[테스트]격자 모양 출력하기](https://www.codetree.ai/trails/complete/curated-cards/test-print-grid-shape)
+
+| 항목 | 내용 |
+|---|---|
+| 분류 | Trail |
+| 커리큘럼 | [Trail 1 / 2차원 배열 / 격자로서의 2차원 배열](https://www.codetree.ai/trail-info/novice-low/) |
+| 난이도 | 쉬움 |
+| 경험치 | 10 XP |
---
+++ 코드트리/trail1/격자 모양 출력하기/print-grid-shape.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+    const int MAX_SIZE = 10;
+
+    int n, m, arr[MAX_SIZE][MAX_SIZE] = {};
+
+    cin >> n >> m;
+
+    for(int i = 0; i < m; i++)
+    {
+        int y, x;
+
+        cin >> y >> x;
+
+        arr[y-1][x-1] = y * x;
+    }
+
+    for(int i = 0; i < n; i++)
+    {
+        for(int j = 0; j < n; j++)
+        {
+            cout << arr[i][j] << ' ';
+        }
+
+        cout << endl;
+    }
+
+    return 0;
+}
