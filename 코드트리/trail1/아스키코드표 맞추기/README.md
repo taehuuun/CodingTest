1b393d695c1101f16591fcd77e0f9b61857e39ee
LimTaeHun <43982651+taehuuun@users.noreply.github.com>
2026-07-27 20:51:14 +0900

    [Passed] 아스키코드표 맞추기 (2ms, 0MB)

---
+++ 코드트리/trail1/아스키코드표 맞추기/README.md
@@ -0,0 +1,8 @@
+# [[개념]아스키코드표 맞추기](https://www.codetree.ai/trails/complete/curated-cards/intro-chart-of-ASCII)
+
+| 항목 | 내용 |
+|---|---|
+| 분류 | Trail |
+| 커리큘럼 | [Trail 1 / 문자열 / 아스키 코드](https://www.codetree.ai/trail-info/novice-low/) |
+| 난이도 | 쉬움 |
+| 경험치 | 10 XP |
---
+++ 코드트리/trail1/아스키코드표 맞추기/chart-of-ASCII.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+    int arr[5] = {};
+
+    for(int i = 0; i < 5; i++)
+    {
+        cin >> arr[i];
+    }
+
+    for(int i = 0 ; i < 5; i++)
+    {
+        cout << (char)arr[i] << ' ';
+    }
+
+    return 0;
+}
